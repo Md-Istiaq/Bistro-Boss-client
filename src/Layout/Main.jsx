fc6a0f77e3940e9b79b7320e1583f6a997b96c1b
MD-Istiaq <mdistiaq102@gmail.com>
2024-10-19 01:16:25 +0600

    footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import Footer from '../Pages/Shared/Footer/Footer';
 function Main() {
   return (
     <div>
         <Outlet></Outlet>
+        <Footer></Footer>
     </div>
   )
 }
