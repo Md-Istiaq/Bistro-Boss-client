be4076c8a39c17fc6ed75754ceb365b5185cd7ee
MD-Istiaq <mdistiaq102@gmail.com>
2024-10-18 21:15:05 +0600

    Router setup and home page created

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet } from "react-router-dom";
+function Main() {
+  return (
+    <div>
+        <Outlet></Outlet>
+    </div>
+  )
+}
+
+export default Main
