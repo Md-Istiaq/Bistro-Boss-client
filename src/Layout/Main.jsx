dcf75ed3495d0e7eb5c079e6ca217874139bedab
MD-Istiaq <mdistiaq102@gmail.com>
2024-10-19 01:34:17 +0600

    Navbar created

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import Footer from '../Pages/Shared/Footer/Footer';
+import NavBar from '../Pages/Shared/NavBar/NavBar';
 function Main() {
   return (
     <div>
-        <Outlet></Outlet>
-        <Footer></Footer>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   )
 }
