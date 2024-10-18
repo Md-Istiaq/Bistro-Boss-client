0a648ea7915f81f3c07ee8c57179d0db8bbbe8b8
MD-Istiaq <mdistiaq102@gmail.com>
2024-10-19 02:57:13 +0600

    Catagory section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
+import Catagory from '../Catagory/Catagory'
 
 function Home() {
   return (
+    <div>
     <Banner></Banner>
+    <Catagory></Catagory>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
+    <div className='max-w-screen-2xl mx-auto'>
     <RouterProvider router={router} />
     </div>
   </StrictMode>,
